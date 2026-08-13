f4cad3e79f88a3f5c3aad5df1e56a2cf2fd0ef3e
DMV22 <maksdivnich12@gmail.com>
2026-08-13 18:57:13 +0300

    feat(store): add redux-persist for league and ui slices

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import App from './App.tsx'
-import { store } from '@/store/index.ts'
+import { persistor, store } from '@/store/index.ts'
 
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,28 @@
 import leagueReducer from '@/features/league/slices/league.slice'
 import uiReducer from '@/features/ui/slices/ui.slice'
 
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  league: leagueReducer,
+  ui: uiReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["league", "ui"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    league: leagueReducer,
-    ui: uiReducer,
-  }
-})
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
